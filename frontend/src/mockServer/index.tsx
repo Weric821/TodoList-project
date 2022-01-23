528daea9537062f83944db616d372948e6683e31
Weric821 <weric3690606@gmail.com>
2022-01-24 00:31:06 +0800

    Update: MockServer factories

--- frontend/src/mockServer/index.tsx
+++ frontend/src/mockServer/index.tsx
@@ -1,7 +1,11 @@
-import { belongsTo, createServer, Factory, hasMany, Model } from "miragejs"
+import { belongsTo, createServer, hasMany, Model, Factory } from "miragejs"
 import { User } from "../type/user"
 import { Todo } from "../type/todo"
 import * as faker from "faker"
+import * as moment from "moment"
+// import { association, Factory, trait } from 'ember-cli-mirage';
+
+
 
 // import faker from 'faker'
 
@@ -13,18 +17,26 @@ export default function MakeServer({ environment = "test" } = {}) {
       user: Model.extend({todo:hasMany()}),
       todo: Model.extend({user:belongsTo()}),
     },
-    // name: string,
-    // email: string,
-    // info?: string,
-    // createTime?: string
 
     factories: {
       user: Factory.extend({
         name: faker.name.firstName()+" "+faker.name.lastName(),
         email:  faker.internet.email(),
-        info: faker.address.country() //TODO: create factories
+        info: faker.address.country(),
+        createTime: moment(faker.date.past(2)).format('LLLL')
       }),
-
+      
+    //   todo: Factory.extend({
+    //     author: trait({
+    //       receiver: 'user',
+    //       user: association()
+    //     }),
+    //     title: faker.address.country(),
+    //     content: faker.address.country(),
+    //     deadline: moment(faker.date.past(2)).format('LLLL'),
+    //     tags: [faker.name.firstName() * 3],
+    //     createTime: moment(faker.date.past(2)).format('LLLL')
+    //   }),
     },
 
     seeds(server) {
@@ -33,8 +45,8 @@ export default function MakeServer({ environment = "test" } = {}) {
         name: "eric",
         email: faker.internet.email(),
       })
-      server.create("user", { name: "Interstellar", year: 2014 })
-      server.create("user", { name: "Dunkirk", year: 2017 })
+      // server.create("user", { name: "Interstellar", year: 2014 })
+      // server.create("user", { name: "Dunkirk", year: 2017 })
     },
 
     routes() {
