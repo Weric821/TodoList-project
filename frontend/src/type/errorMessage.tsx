d0dc97abb8a2d43c2f75d1795825807ac93d3c31
Weric821 <weric3690606@gmail.com>
2022-02-02 00:58:06 +0800

    Update: Refactor errorMessage type with io-ts

--- frontend/src/type/errorMessage.tsx
+++ frontend/src/type/errorMessage.tsx
@@ -1,4 +1,9 @@
-export type ErrorMessage = {
-    type: string,
-    message: string
-}
+import * as t from "io-ts"
+
+export const ErrorMessageCodec = t.type({
+  type: t.string,
+  message: t.string,
+})
+
+// ErrorMessage
+export type ErrorMessage = t.TypeOf<typeof ErrorMessageCodec>
